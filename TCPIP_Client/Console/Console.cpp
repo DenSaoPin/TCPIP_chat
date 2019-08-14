b31453fe61db181218a9c2248678719098c13757
Unknown <densaopin90@gmail.com>
2019-08-14 15:49:40 +0300

    Implemented soft closing for Console

--- TCPIP_Client/Console/Console.cpp
+++ TCPIP_Client/Console/Console.cpp
@@ -6,53 +6,64 @@
 #include <string>
 #include <future>
 
-void OnRecieveMessage(const char *szName, const int* messageType, const char *szMessage);
-
-// OnSendMessage(const char *szMessage);
-
-int main()
+namespace Console
 {
-	std::string message;
-	std::cin >> message;
-	SetConnectionParams(message.c_str(), "127.0.0.1", "7700");
-	setCallbackMessageReceived(OnRecieveMessage);
+	void OnRecieveMessage(const char *szName, const int* messageType, const char *szMessage);
+	void OnExit();
 
-	std::thread chatThread(ClientMainLoop);
+	std::thread chatThread;
 
-	//if (!asyncThread.valid())
-	//{
-	//	asyncThread = std::async(std::launch::async, []
-	//	{
-	//		std::string s = "";
-	//		if (std::getline(std::cin, s)) return s;
-	//	});
-	//}
-
-	//std::string str;
-	//if (asyncThread.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
-	//{
-	//	str = asyncThread.get();
-
-	//	asyncThread = std::async(std::launch::async, []
-	//	{
-	//		std::string s = "";
-	//		if (std::getline(std::cin, s)) return s;
-	//	});
-	//}
-	//else
-	//{
-	//	//str = " Test message \n";
-	//}
-
-	while (true)
+	int main()
 	{
-		std::getline(std::cin, message);
-		ClientSendMessage(message.c_str());
+		std::string message;
+		std::cin >> message;
+		SetConnectionParams(message.c_str(), "127.0.0.1", "7700");
+		setCallbackMessageReceived(OnRecieveMessage);
+
+		//chatThread = std::thread(ClientMainLoop);
+		//atexit(OnExit);
+
+		//if (!asyncThread.valid())
+		//{
+		//	asyncThread = std::async(std::launch::async, []
+		//	{
+		//		std::string s = "";
+		//		if (std::getline(std::cin, s)) return s;
+		//	});
+		//}
+
+		//std::string str;
+		//if (asyncThread.wait_for(std::chrono::seconds(1)) == std::future_status::ready)
+		//{
+		//	str = asyncThread.get();
+
+		//	asyncThread = std::async(std::launch::async, []
+		//	{
+		//		std::string s = "";
+		//		if (std::getline(std::cin, s)) return s;
+		//	});
+		//}
+		//else
+		//{
+		//	//str = " Test message \n";
+		//}
+
+		while (true)
+		{
+			std::getline(std::cin, message);
+			ClientSendMessage(message.c_str());
+		}
 	}
-}
 
+	void OnRecieveMessage(const char *szName, const int* messageType, const char *szMessage)
+	{
+		printf("%s: %s" "\n", szName, szMessage);
+	}
 
-void OnRecieveMessage(const char *szName, const int* messageType, const char *szMessage)
-{
-	printf("%s: %s",szName, szMessage);
+	void OnExit()
+	{
+		ClientTerminate();
+
+		chatThread.join();
+	}
 }
