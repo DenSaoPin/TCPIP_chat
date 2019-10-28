70f7f6d3c75757a2387b08d17b6de1d628a817d8
DenSaoPin <densaopin90@gmail.com>
2019-10-28 16:43:50 +0300

    implemented EMessageType enum

--- TCPIP_Client/QT_UI/qt/mainwindow.cpp
+++ TCPIP_Client/QT_UI/qt/mainwindow.cpp
@@ -26,3 +26,35 @@ void MainWindow::PrintMessage(QString& qstr)
 {
     ui->textBox_Out->append(qstr + "\n");
 }
+
+void MainWindow::on_button_Send_clicked()
+{
+    QString text = ui->textBox_input->toPlainText();
+    PrintMessage(text);
+
+    QByteArray ba = text.toLocal8Bit();
+    const char *szMessage = ba.data();
+
+    ClientSendMessage(nullptr, (const int)EMessageType::eBroadcastMessage, szMessage, ba.length());
+
+    ui->textBox_input->clear();
+}
+
+void MainWindow::on_actionReconnect_triggered()
+{
+    QString rec("@reconnect@");
+/*
+    if (m_clientStatus == EClientStatus::eTerminating)
+    {
+    }
+    else if (m_clientStatus == EClientStatus::eShutDown)
+    {
+    }
+    else
+    {
+        ClientTerminate();
+    }
+    QThread::usleep(500);
+*/
+    //m_clientDllMainThread = std::async(std::launch::async, ClientMainLoop);
+}
